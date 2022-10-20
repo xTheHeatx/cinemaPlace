69e8dc3387683803420a9678dd9766f69e2856a6
Medicino <105019072+xTheHeatx@users.noreply.github.com>
2022-10-20 18:35:48 +0400

    added cards and hover effects

--- js/apiLoad.js
+++ js/apiLoad.js
@@ -1,29 +1,73 @@
-let url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1'
+// let response = fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1',
+//     {
+//         method: "GET",
+//         headers: {
+//             "X-API-KEY": "8c8e1a50-6322-4135-8875-5d40a5420d86"
+//         }
+//     })
+
+// let result = response.json()
+
+// // let movieName = result.films[0].nameRu
+
+// console.log(result)
 
 
-fetch(url, {
-    method: "GET",
-    headers: {
-        "X-API-KEY": "8c8e1a50-6322-4135-8875-5d40a5420d86"
-    }
+// const apiLoad = window.onload = () => {
 
-})
+//     const wrapper = document.querySelector('.movies__wrapper');
+
+// const cardHTML = `<div class="movies__movie-card movie-card" data-id="${item.filmId}">
+//             <div class="movie-card__img-block">
+//                 <img class="movie-card__img" src="${item.posterUrlPreview}" alt="${item.nameRu}" >
+//             </div>
+//             <h2 class="movie-card__title">${item.nameRu}</h2>
+//             <h2 class="movie-card__genre">${item.genres[0, 1, 2, 3]}</h2>
+//         </div>`
+// wrapper.insertAdjacentHTML('beforeEnd', cardHTML)
 
 
-const apiLoad = window.onload = function cardInsert() {
-    const wrapper = document.querySelector('.movies__wrapper');
-    wrapper.insertAdjacentHTML('beforeEnd', `<div class="movies__movie-card movie-card">
-    <div class="movie-card__img-block">
-        <img src="" alt="" class="">
-    </div>
-    <h2 class="movie-card__title">TITLE TITLE TITLE TITLE</h2>
-    <h2 class="movie-card__genre">GENRE GENRE GENRE GENRE</h2>
-</div>`)
 
-    console.log(wrapper)
+// }
+
+
+
+const apiLoad = document.querySelector('.movies__wrapper');
+
+
+getProducts();  // Запускаем getProducts
+
+
+async function getProducts() {   // Асинхронная функция получения данных из файла products.json
+
+    const response = await fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1',
+        {
+            method: "GET",
+            headers: {
+                "X-API-KEY": "8c8e1a50-6322-4135-8875-5d40a5420d86"
+            }
+        });  // Получаем данные из products.json
+
+    const result = await response.json();   // Парсим данные из JSON формата в JS
+
+    console.log(result.films[0].nameRu)
+
+    renderProducts(productsArray);   // Запускаем ф-ю рендера (отображения товаров)
+
 }
 
-
+function renderProducts(productsArray) {
+    productsArray.forEach(function (item) {
+        const productHTML = `<div class="movies__movie-card movie-card" data-id="${item.filmId}">
+                                <div class="movie-card__img-block">
+                                    <img class="movie-card__img" src="${item.posterUrlPreview}" alt="${item.nameRu}">
+                                </div>
+                                <h2 class="movie-card__title">${item.nameRu}</h2>
+                                <h2 class="movie-card__genre">${item.genres[0, 1, 2, 3]}</h2>
+                            </div>`
+        productsContainer.insertAdjacentHTML('beforeend', productHTML);
+    });
+}
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
-import apiLoad from "./apiLoad.js";
+
+
+
+
+getMovie();  // Запускаем getProducts
+
+
+async function getMovie() {   // Асинхронная функция получения данных из файла products.json
+
+    const response = await fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1',
+        {
+            method: "GET",
+            headers: {
+                "X-API-KEY": "8c8e1a50-6322-4135-8875-5d40a5420d86"
+            }
+        });  // Получаем данные из products.json
+
+    const result = await response.json();   // Парсим данные из JSON формата в JS
+
+
+    renderCards(result);   // Запускаем ф-ю рендера (отображения товаров)
+
+}
+
+function renderCards(data) {
+    const wrapper = document.querySelector('.movies__wrapper');
+
+    data.films.forEach((item) => {
+        const card = document.createElement("div")
+
+        card.innerHTML = `<div class="movies__movie-card movie-card">
+                                <div class="movie-card__img-block">
+                                    <a className="movie-card__film-link" href="${item.posterUrl}">
+                                     <img class="movie-card__img" src="${item.posterUrlPreview}" alt="${item.nameRu}">
+                                     </a>
+                                </div>
+                                <h2 class="movie-card__title">${item.nameRu}</h2>
+                                <h2 class="movie-card__genre">${item.genres.map(genre => ` ${genre.genre}`)}</h2>
+                            </div>`;
+
+        wrapper.appendChild(card)
+    });
+}
+
+
+
+
+
