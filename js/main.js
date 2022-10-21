4c4c7b6bf5c16d1e61e8730c3943d3389465f1b4
Medicino <105019072+xTheHeatx@users.noreply.github.com>
2022-10-21 10:57:37 +0400

    !

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
 
+const API_KEY = "8c8e1a50-6322-4135-8875-5d40a5420d86"
+
+const GET_MOVIE_INFO = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1'
+
+let SEARCH_API_URL = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
+
+getMovie(GET_MOVIE_INFO);
 
 
 
-getMovie();  // Запускаем getProducts
 
+async function getMovie(url) {
 
-async function getMovie() {   // Асинхронная функция получения данных из файла products.json
-
-    const response = await fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1',
+    const response = await fetch(url,
         {
             method: "GET",
             headers: {
-                "X-API-KEY": "8c8e1a50-6322-4135-8875-5d40a5420d86"
+                "Content-Type": "application/json",
+                "X-API-KEY": API_KEY
             }
-        });  // Получаем данные из products.json
+        });
 
-    const result = await response.json();   // Парсим данные из JSON формата в JS
+    const result = await response.json();
 
 
-    renderCards(result);   // Запускаем ф-ю рендера (отображения товаров)
+    renderCards(result);   // Запускаем ф-ю рендера (отображения)
 
 }
 
 function renderCards(data) {
     const wrapper = document.querySelector('.movies__wrapper');
+
+    document.querySelector('.movies__wrapper').innerHTML = ""
 
     data.films.forEach((item) => {
         const card = document.createElement("div")
@@ -46,6 +54,19 @@ function renderCards(data) {
     });
 }
 
+const form = document.querySelector('form');
+const search = document.querySelector('.header__search');
+
+form.addEventListener('submit', (def) => {
+    def.preventDefault()
+
+    const apisearch = `${SEARCH_API_URL}${search.value}`
+    if (search.value) {
+        getMovie(apisearch)
+
+        search.value = ""
+    }
+})
 
 
 
